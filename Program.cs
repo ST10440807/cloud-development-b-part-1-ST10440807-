707e5272b25b287abcdb3b1d10c72f0aef19e9c1
ST10440807 <161421494+ST10440807@users.noreply.github.com>
2025-08-28 21:16:34 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,63 @@
+using ABCRetailers.Services;
+
+namespace ABCRetailers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var configuration = builder.Configuration;
+
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+
+            //Register tablestorage with configuration
+            builder.Services.AddSingleton(new TableStorageService(configuration.GetConnectionString("AzureStorage")));
+
+            //Register Blobservice with configuration
+            builder.Services.AddSingleton(new BlobService(configuration.GetConnectionString("AzureStorage")));
+
+            //Register queue storage
+            builder.Services.AddSingleton<QueueService>(sp =>
+            {
+                var connectionString = configuration.GetConnectionString("AzureStorage");
+                return new QueueService(connectionString, "orders");
+
+            });
+
+            //Fileshare with configuraton
+            builder.Services.AddSingleton<AzureFileShareService>(sp =>
+            {
+                var connectionString = configuration.GetConnectionString("AzureStorage");
+                return new AzureFileShareService(connectionString, "productshare");
+            });
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapStaticAssets();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}")
+                .WithStaticAssets();
+
+            app.Run();
+        }
+    }
+}
